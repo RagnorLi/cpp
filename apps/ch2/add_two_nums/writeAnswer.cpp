0c2512b5d808373b41c79a8932f5e8331b1869d5
RagnorLi <ragnor.li@outlook.com>
2026-09-07 21:51:05 +0800

    feat:ch3 i know how to write functional cpp code

---
+++ apps/ch2/add_two_nums/writeAnswer.cpp
@@ -0,0 +1,8 @@
+#if 0
+#include "writeAnswer.h"
+#include <iostream>
+
+void writeAnswer(int num){
+    std::cout << "the answer is : " << num << std::endl;
+}
+#endif
---
+++ apps/ch2/marco_define_scope/main.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include "function.h"
+
+// void doSomething(); // forward declare
+
+#define PRINT
+
+int main()
+{
+    doSomething();
+
+    return EXIT_SUCCESS;
+}
